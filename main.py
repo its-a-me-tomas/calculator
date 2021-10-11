fd27322d6492056d176c7cd1bcf82ef3f770e5ec
programer-kun <toacsa33@gmail.com>
2021-10-11 18:16:50 -0500

    Inicio del programa

---
+++ main.py
@@ -0,0 +1,23 @@
+#library
+from scilab import scilab
+
+from tkinter import *
+from tkinter import ttk
+from math import *
+#window's properties
+root = Tk()
+root.iconbitmap('Calculator.ico')
+root.title('Calculadora de Sistemas de ecuaciones')
+root.resizable(0,0)
+root.geometry("720x420")
+#extra settings
+root.config()
+
+style = ttk.Style(root)
+style.theme_use("winnative")
+#end window's properties
+ 
+frame = Frame(root)
+
+root.mainloop()
+print ('hello world')
