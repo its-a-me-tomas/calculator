9acde575728fbe199e84f1ba88e5259a8ca6c568
programer-kun <toacsa33@gmail.com>
2021-11-18 15:01:47 -0600

    venv and other things

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#!/venv/bin/python
 # invocar a los archivos para los métodos
 from seidel import *
 from secante import *
 #librerias para la interfaz grafica (aun trabajando)
 from tkinter import *
+from tkinter.ttk import *
 """
 #Opciones para la calculadora *consola*
 inp = int(input("solución de sistema de ecuaciones(2) o raiz de ecuación no lineal(1)->"))
@@ -26,12 +28,17 @@ elif inp == 2 :
 #INICIALIZAR#
 root = Tk()
 root.withdraw()
-backg = PhotoImage(file = "img/back1.png")
+root.wm_attributes('-transparentcolor','#d1cfcf')
+backg, back2 = PhotoImage(file = "img/back1.png"), PhotoImage(file = "img/back2.png")
 current_window  = None
-
+style = Style()
+style.theme_use(themename='clam')
+style.configure('TButton', font=('Corbel', -12))
+style.configure('TButton', justify='center')
 ####   windows   ####
+
 def windowsys(): #sistema de ecuaciones#
-    global current_window
+    global current_window, back2
     if current_window is not None:
         current_window.destroy()
     current_window = Toplevel(root)
@@ -39,13 +46,17 @@ def windowsys(): #sistema de ecuaciones#
     window = current_window
     #properties
     window.iconbitmap('img/Calculator.ico')
-    window.title('Calculadora de Sistemas de ecuaciones no lineales')
+    window.title('Calculadora')
     window.resizable(0,0)
     window.geometry("720x420")
+    #image background#
+    canva = Canvas(window, width = 720, height = 420)
+    canva.pack(fill = 'both', expand = True)
+    canva.create_image(0,0, image = back2, anchor = 'nw')
     #extra settings
-    window.config(cursor="pirate")
+    window.config(cursor="circle")
     #mensaje
-    asd = Label(window, text="¡Hola,Bienvenido a la Calculadora de sistema de ecuaciones y sistemas no lineales!")
+    asd = Label(window, text="Calculadora de sistemas de ecuaciones")
     asd.place(x=130, y=50, width=450, height=20)
     #entrada de datos
     matrix = Entry(window, width=50)
@@ -56,38 +67,47 @@ def windowsys(): #sistema de ecuaciones#
     error.place(x=130, y=260, width=50)
     Iteraciones = Entry(window, width=50)
     Iteraciones.place(x=130, y=290, width=50)
-    #llamada a function#
-    
+    #get data# #llamada a function#
+    a,b,e,i = converA(matrix.get()), converB(independiente.get()), float(error.get()), int(Iteraciones.get())
+    active = Button(window, text="Calcular raiz", command = lambda: siedel(a, b, e, i))    
+    active.place(x=255,y=300)
     #ventana de secante o sistema
     window.mainloop()
 
+
 def windowequ():  #ecuaciones no lineales#
-    global current_window
+    global current_window, back2
     if current_window is not None:
         current_window.destroy()
     current_window = Toplevel(root)
     current_window.wm_protocol("WM_DELETE_WINDOW", mainWindow)
     window = current_window
     window.iconbitmap('img/Calculator.ico')
-    window.title('Calculadora de sistemas de ecuaciones')
+    window.title('Calculadora')
     window.resizable(0,0)
     window.geometry("720x420")
+    #image background#
+    canva = Canvas(window, width = 720, height = 420)
+    canva.pack(fill = 'both', expand = True)
+    canva.create_image(0,0, image = back2, anchor = 'nw')
     #extra settings
-    window.config(cursor="pirate")
+    window.config(cursor="circle")
     #mensaje
-    asd = Label(window, text="¡Hola,Bienvenido a la Calculadora de sistema de ecuaciones!")
+    asd = Label(window, text="Calculadora de ecuaciones no lineales")
     asd.place(x=130, y=50, width=450, height=20)
     #entrada de datos
     function = Entry(window, width=50)
     function.place(x=130, y=200, width=50)
     intervaloA = Entry(window, width=50)
-    intervaloA.place(x=130, y=230, width=50)
+    intervaloA.place(x=130, y=230,width=50)
     intervaloB = Entry(window, width=50)
     intervaloB.place(x=130, y=260, width=50)
     Error = Entry(window, width=50)
     Error.place(x=130, y=290, width=50)
-    #llamada a function#
-
+    #get data#
+    func,a,b,e = function.get(), float(intervaloA.get()), float(intervaloB.get()), float(Error.get())
+    active = Button(window, text="Calcular raiz", command = lambda: sec(func, a, b, e))    
+    active.place(x=255,y=300)
     ##
     window.mainloop()
 
@@ -103,19 +123,27 @@ def mainWindow():
     window.resizable(0,0)
     window.geometry("260x320")
     #extra settings
-    window.config(cursor="pirate")
+    window.config(cursor="circle")
     #image background
     canva = Canvas(window, width = 260, height = 320)
     canva.pack(fill = 'both', expand = True)
     canva.create_image(0,0, image = backg, anchor = 'nw')
     #botones para elegir
-    pto = Button(window, text="Calculadora \n Sistema de ecuaciones", command=windowsys, font = 'Corbel -15')
-    pto2 = Button(window, text="Calculadora \n Ecuaciones no lineales", command=windowequ, font = 'Corbel -15')
-    pto.place(x=55, y=145)
-    pto2.place(x=55, y=215)
+    system = Button(window, 
+                text="Calculadora\nSistema de ecuaciones",
+                command=windowsys)
+    equation = Button(window,
+                text="Calculadora\nEcuaciones no lineales",
+                command=windowequ)
+    system.place(x=65, y=145)
+    equation.place(x=65, y=215)
     #mensaje
-    asd = Label(window, text = "Bienvenido!", font = 'Corbel -35 bold')
-    asd.place(x=35, y=35, width=200, height=60)
+    asd = Label(window,
+                text = "Bienvenido!",
+                font = 'Corbel -25 bold',
+                background = '#d1cfcf',
+                justify='center')
+    asd.place(x=65, y=35)
     #ventana de secante o sistema
     window.mainloop()
 
