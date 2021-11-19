df790c5580ecbd74fef2234e9c2e7f5f56eff70c
programer-kun <toacsa33@gmail.com>
2021-11-19 14:17:33 -0600

    finish UI system

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/venv/bin/python
 # invocar a los archivos para los métodos
 from seidel import *
 from secante import *
@@ -28,17 +27,27 @@ elif inp == 2 :
 #INICIALIZAR#
 root = Tk()
 root.withdraw()
-root.wm_attributes('-transparentcolor','#d1cfcf')
-backg, back2 = PhotoImage(file = "img/back1.png"), PhotoImage(file = "img/back2.png")
-current_window  = None
+root.wm_attributes('-transparentcolor','#979191')
+#style
 style = Style()
 style.theme_use(themename='clam')
-style.configure('TButton', font=('Corbel', -12))
+style.configure('TButton', font=('Candara', -12))
 style.configure('TButton', justify='center')
+style.configure('TLabel', font=('Candara',-13))
+#global var
+backg, back2 = PhotoImage(file = "img/back1.png"), PhotoImage(file = "img/back2.png")
+
+current_window, result =  None, StringVar()
+
 ####   windows   ####
+    
+def DataEquation(func,a,b,e):
+    secante(func,a,b,e)
+
 
 def windowsys(): #sistema de ecuaciones#
-    global current_window, back2
+    global current_window, back2, result
+    result.set('')
     if current_window is not None:
         current_window.destroy()
     current_window = Toplevel(root)
@@ -56,21 +65,49 @@ def windowsys(): #sistema de ecuaciones#
     #extra settings
     window.config(cursor="circle")
     #mensaje
-    asd = Label(window, text="Calculadora de sistemas de ecuaciones")
-    asd.place(x=130, y=50, width=450, height=20)
+    asd = Label(window,
+                text="Calculadora de sistemas de ecuaciones por el método Gauss-Seidel",
+                font="Candara -20 bold")
+    asd.place(x=80, y=20)
     #entrada de datos
-    matrix = Entry(window, width=50)
-    matrix.place(x=130, y=200, width=50)
-    independiente = Entry(window, width=50)
-    independiente.place(x=130, y=230, width=50)
-    error = Entry(window, width=50)
-    error.place(x=130, y=260, width=50)
-    Iteraciones = Entry(window, width=50)
-    Iteraciones.place(x=130, y=290, width=50)
+
+    mat = Label(window, 
+                text="Matrix de Coeficientes del sistema\nEjemplo: -7 -4 -1 -1;-1 8 -5 0;-1 -2 -4 0;0 2 0 -6",
+                justify='center')
+    matrix = Entry(window, width=40)
+    mat.place(x=47,y=80)
+    matrix.place(x=40, y=115)
+    
+    ind = Label(window,
+                text = 'Términos independientes\nEjemplo: -10;35;3;-6',
+                justify='center')
+    independiente = Entry(window)
+    ind.place(x=75,y=140)
+    independiente.place(x=85, y=175)
+    
+    er = Label(window,
+                text = 'Error permitido\nEjemplo: 0.001',
+                justify='center')
+    error = Entry(window)
+    er.place(x=100,y=200)
+    error.place(x=85, y=235)
+    
+    ite = Label(window,
+                text='Numero de iteraciones\nEjemplo: 20',
+                justify ='center')
+    Iteraciones = Entry(window)
+    ite.place(x=83,y=260)
+    Iteraciones.place(x=85, y=295)
+    
+    impresion = Label(window,textvariable=result,background = '#979191')
     #get data# #llamada a function#
-    a,b,e,i = converA(matrix.get()), converB(independiente.get()), float(error.get()), int(Iteraciones.get())
-    active = Button(window, text="Calcular raiz", command = lambda: siedel(a, b, e, i))    
-    active.place(x=255,y=300)
+    active = Button(window,
+        text="Calcular raiz",
+        command = lambda: result.set(siedel(converA(matrix.get()), converB(independiente.get()), float(error.get()), int(Iteraciones.get()))))
+    active.place(x=95,y=330)
+    impresion.place(x=360 ,y= 70)
+    
+    
     #ventana de secante o sistema
     window.mainloop()
 
@@ -105,8 +142,9 @@ def windowequ():  #ecuaciones no lineales#
     Error = Entry(window, width=50)
     Error.place(x=130, y=290, width=50)
     #get data#
-    func,a,b,e = function.get(), float(intervaloA.get()), float(intervaloB.get()), float(Error.get())
-    active = Button(window, text="Calcular raiz", command = lambda: sec(func, a, b, e))    
+    active = Button(window,
+    text="Calcular raiz",
+    command = lambda: DataEquation(function.get(), float(intervaloA.get()), float(intervaloB.get()), float(Error.get())))    
     active.place(x=255,y=300)
     ##
     window.mainloop()
@@ -140,8 +178,8 @@ def mainWindow():
     #mensaje
     asd = Label(window,
                 text = "Bienvenido!",
-                font = 'Corbel -25 bold',
-                background = '#d1cfcf',
+                font = 'Candara -25 bold',
+                background = '#979191',
                 justify='center')
     asd.place(x=65, y=35)
     #ventana de secante o sistema
--- seidel.py
+++ seidel.py
@@ -1,26 +1,30 @@
 #-7 -4 -1 -1;-1 8 -5 0;-1 -2 -4 0;0 2 0 -6 || -10;35;3;-36 ejemplo de entrada
 #lib for graphics
+import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 """ 
 Argumentos:
 a = matrix de coeficientes separados por espacios y punto y coma
 b = vector de terminos independientes separados por punto y coma
 e = error permitido
-maxI = maximo de iteraciones """
+maxI = maximo de iteraciones 
+"""
 
 def siedel(a,b,e,maxI): #definición del método
     x0,x1=[],[]
     num = len(a)
     for i in range(num): #vector de aproximación auxiliar
         x0.append(0)
-    k, err, imp, x1 = 0, 1, "k\t", x0.copy() #variables auxiliares
+    k, err, imp, x1, text= 0, 1, "k\t", x0.copy(),"" #variables auxiliares
     for i in range(num): #impresión de la tabla
-        imp +=  "x"+str(i+1)+"\t\t"
+        imp +=  "x"+str(i+1)+"\t"
     print(imp+"error")
     print("------------------------------------------------\n")
+    text += imp + "error\n"
     ######Método de Gauss-Seidel#######
     while err>e:
-        k = k+1
+        k += 1
         impre = str(k)+"\t"
         for i in range(0,num):
             suma = b[i]
@@ -33,10 +37,21 @@ def siedel(a,b,e,maxI): #definición del método
         x1=x0.copy()
         impre += "{:.5f}".format(err)+"\n"
         print(impre)
+        text += impre
         if k > maxI:
             print("El método no converge")
+            if len(a[0]) > 3:
+                text += "\nMás de tres dimensiones, no hay gráfica"
+            else:
+                graphic(len(a[0]),a,b)
+            return "El método no converge"
             break
     ######FIN MÉTODO GAUSS-SEIDEL#######
+    if len(a[0]) > 3:
+        text += "\nMás de tres dimensiones, no hay gráfica"
+    else:
+        graphic(len(a[0]),a,b)
+    return text
     
 #conversor de string a matrix
 def converA(coe):
@@ -76,3 +91,22 @@ def converB(array):
             sr = ""
     #print(arreglo)
     return arreglo
+
+#grafica#
+#grafica dinamica para n ecuaciones#
+def graphic(dimension, matrix, independet):
+    if dimension == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(111,projection="3d")
+        x, y = np.linspace(-8,8,100), np.linspace(-8,8,100)
+        X, Y = np.meshgrid(x,y)
+        for i in range(len(matrix)):
+            z = (independet[i] - matrix[i][0]*X - matrix[i][1]*Y)/matrix[i][2]
+            ax.plot_surface(X,Y,z, facecolors='b', alpha=0.5, rstride=100, cstride=100)
+        plt.show()
+    elif dimension == 2:
+        x, y = np.linspace(-10,10,500), np.linspace(-10,10,500)
+        for i in range(len(matrix)):
+            y = (independet[i]-matrix[i][0]*x)/matrix[i][1]
+            plt.plot(x,y)
+        plt.show()    
